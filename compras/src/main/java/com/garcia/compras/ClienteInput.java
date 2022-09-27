5d8d2729db34323316efecafd09c79742131ccbf
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2022-09-27 15:49:53 -0300

    Removendo anotações do lombok

--- compras/src/main/java/com/garcia/compras/ClienteInput.java
+++ compras/src/main/java/com/garcia/compras/ClienteInput.java
@@ -1,13 +1,8 @@
 package com.garcia.compras;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ClienteInput {
 
   private Long id;
--- compras/src/main/java/com/garcia/compras/ProdutoInput.java
+++ compras/src/main/java/com/garcia/compras/ProdutoInput.java
@@ -1,12 +1,8 @@
 package com.garcia.compras;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ProdutoInput {
 
   private Long id;
